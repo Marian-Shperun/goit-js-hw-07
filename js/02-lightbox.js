efc794964800cb45294f04ad346de468f8eddfad
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-07-31 21:11:39 +0200

    add lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(2);
+
+const containerGallery = document.querySelector("ul.gallery");
+
+console.log(SimpleLightbox);
+galleryItems.map((item) => {
+  const galleryItem = document.createElement("a");
+  galleryItem.classList.add("gallery__item");
+  galleryItem.href = item.original;
+
+  const imgEl = document.createElement("img");
+  imgEl.classList.add("gallery__image");
+  imgEl.src = item.preview;
+  // imgEl.dataset.source = item.original;
+  imgEl.alt = item.description;
+
+  galleryItem.append(imgEl);
+  containerGallery.append(galleryItem);
+});
+
+const gallery = new SimpleLightbox(".gallery .gallery__item");
